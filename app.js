6da150eb46619e64f36e84cef7f3fac01f5a9511
Saad Tajwar <saads@ad.unc.edu>
2023-06-25 22:59:34 -0400

    New version!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
